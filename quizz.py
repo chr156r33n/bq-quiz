c99fd06b98db29433fb16e4939f0520545f39eab
Chris Green <75560394+chr156r33n@users.noreply.github.com>
2025-03-05 07:08:31 +0000

    Update quizz.py

--- quizz.py
+++ quizz.py
@@ -2,10 +2,6 @@ import streamlit as st
 import pandas as pd
 import os
 
-# Set Font
-with open( "/mount/src/bq-quiz/quizz.py" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
-
 # Set page config for embedding
 st.set_page_config(layout="centered", initial_sidebar_state="collapsed")
 
